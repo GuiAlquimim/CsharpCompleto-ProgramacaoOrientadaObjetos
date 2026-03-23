58a46228bd048bde3304781065f1512fa459a579
GuiAlquimim <galquimim04@gmail.com>
2026-03-23 18:29:01 -0300

    Exercícios propostos (classes, atributos e métodos) concluídos

---
+++ Secao4-ClassAtribMet/ExFixacao3-ClassAtribMet/ExFixacao3-ClassAtribMet/Aluno.cs
@@ -0,0 +1,23 @@
+﻿namespace ExFixacao3_ClassAtribMet
+{
+    internal class Aluno
+    {
+        private string Nome { get; set; }
+        private double Nota1 { get; set; }
+        private double Nota2 { get; set; }
+        private double Nota3 { get; set; }
+
+        public Aluno(string nome, double nota1, double nota2, double nota3)
+        {
+            Nome = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+
+        public double CalcularNota()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+    }
+}
---
+++ Secao4-ClassAtribMet/ExFixacao3-ClassAtribMet/ExFixacao3-ClassAtribMet/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExFixacao3_ClassAtribMet
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas:");
+            double nota1 = double.Parse(Console.ReadLine());
+            double nota2 = double.Parse(Console.ReadLine());
+            double nota3 = double.Parse(Console.ReadLine());
+
+            Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
+
+            double notaFinal = aluno.CalcularNota();
+
+            Console.WriteLine($"Nota final = {notaFinal.ToString("f2")}");
+
+            if (notaFinal < 60)
+            {
+                Console.WriteLine("Reprovado!");
+                Console.WriteLine($"Faltaram {(60 - notaFinal).ToString("f2")} pontos");
+            }
+            else
+                Console.WriteLine("Aprovado!");
+        }
+    }
+}
