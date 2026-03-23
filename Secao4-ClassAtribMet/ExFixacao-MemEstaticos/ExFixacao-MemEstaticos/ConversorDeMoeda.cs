1966f668bcba927b5da96028433c386d8aa60124
GuiAlquimim <galquimim04@gmail.com>
2026-03-23 18:57:42 -0300

    Exercício de fixação de membros estáticos realizado. Seção 4 finalizada!

---
+++ Secao4-ClassAtribMet/ExFixacao-MemEstaticos/ExFixacao-MemEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExFixacao_MemEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        static double IOF = 0.06;
+
+        public static double ValorCotacao(double cotacao, double quantidade)
+        {
+            double taxa = cotacao * quantidade * IOF;
+            return cotacao * quantidade + taxa;
+        }
+    }
+}
---
+++ Secao4-ClassAtribMet/ExFixacao-MemEstaticos/ExFixacao-MemEstaticos/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExFixacao_MemEstaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Valor a ser pago em reais = {ConversorDeMoeda.ValorCotacao(cotacao, quantidade).ToString("f2")}");
+        }
+    }
+}
