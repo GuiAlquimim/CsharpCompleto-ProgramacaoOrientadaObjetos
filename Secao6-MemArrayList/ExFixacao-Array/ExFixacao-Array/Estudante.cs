3be9c9acd26153b432afcc5e9ff099c2d172bf21
GuiAlquimim <galquimim04@gmail.com>
2026-03-25 09:28:54 -0300

    Exercício de fixação sobre vetores concluído

---
+++ Secao6-MemArrayList/ExFixacao-Array/ExFixacao-Array/Estudante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExFixacao_Array
+{
+    internal class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
---
+++ Secao6-MemArrayList/ExFixacao-Array/ExFixacao-Array/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ExFixacao_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] quartos = new Estudante[10];
+
+            Console.Write("Quantos estudantes vão alugar um quarto? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nome do estudante: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email do estudante: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto alugado: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                quartos[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine("Quartos ocupados no pensionado:");
+
+            for (int i = 0; i < quartos.Length; i++)
+            {
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine($"[{i}] ocupado por {quartos[i].Nome}, contato: {quartos[i].Email}");
+                }
+            }
+        }
+    }
+}
