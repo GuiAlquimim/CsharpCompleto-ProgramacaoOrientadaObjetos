500732a6de5d8f0afb2e735649935129e1cadcdb
GuiAlquimim <galquimim04@gmail.com>
2026-03-26 07:55:05 -0300

    Exercício de fixação sobre litas concluído.

---
+++ Secao6-MemArrayList/ExFixacao-List/ExFixacao-List/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace ExFixacao_List
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            double increase = Salary * percentage / 100;
+            Salary += increase;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Salary: {Salary.ToString("f2")}";
+        }
+    }
+}
---
+++ Secao6-MemArrayList/ExFixacao-List/ExFixacao-List/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ExFixacao_List
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("How many employees? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                employees.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id to increase salary: ");
+            int idToIncrease = int.Parse(Console.ReadLine());
+
+            Employee employeeAux = employees.Find(x => x.Id == idToIncrease);
+
+            if (employeeAux != null)
+            {
+                Console.Write("Percentage salary increase: ");
+                int percentage = int.Parse(Console.ReadLine());
+                employeeAux.increaseSalary(percentage);
+            }
+            else
+                Console.WriteLine("Id not found");
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employess:");
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
