ef50d21a9c04bb1a6bed8f9cf33935e82a91a68f
GuiAlquimim <galquimim04@gmail.com>
2026-04-06 19:04:59 -0300

    Exercício de fixação sobre tratamento de exceções concluído. Seção 10 finalizada!

---
+++ Secao10-Excecoes/ExFixacao-Excecoes/ExFixacao-Excecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace ExFixacao_Excecoes.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Secao10-Excecoes/ExFixacao-Excecoes/ExFixacao-Excecoes/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExFixacao_Excecoes.Entities;
+using ExFixacao_Excecoes.Entities.Exceptions;
+
+namespace ExFixacao_Excecoes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine());
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+
+                account.Withdraw(amount);
+
+                Console.WriteLine($"New balance: {account.Balance.ToString("f2")}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Error while attempting withdrawal: {e.Message}");
+            }
+        }
+    }
+}
