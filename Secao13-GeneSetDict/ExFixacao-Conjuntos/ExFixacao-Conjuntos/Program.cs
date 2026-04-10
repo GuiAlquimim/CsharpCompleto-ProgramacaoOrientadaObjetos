cfb64a60f4aaf3a872fbf4ba48364cb6b044ae9e
GuiAlquimim <galquimim04@gmail.com>
2026-04-10 19:30:25 -0300

    Exercício de fixação sobre conjuntos concluído

---
+++ Secao13-GeneSetDict/ExFixacao-Conjuntos/ExFixacao-Conjuntos/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExFixacao_Conjuntos
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HashSet<int> alunos = new HashSet<int>();
+            char[] cursos = { 'A', 'B', 'C' };
+
+            foreach (char curso in cursos)
+            {
+                Console.Write($"Quantos aluos irão se matricular no curso {curso}? ");
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write($"#{i}: ");
+                    int aluno = int.Parse(Console.ReadLine());
+                    alunos.Add(aluno);
+                }
+            }
+
+            Console.WriteLine($"Total students: {alunos.Count}");
+        }
+    }
+}
