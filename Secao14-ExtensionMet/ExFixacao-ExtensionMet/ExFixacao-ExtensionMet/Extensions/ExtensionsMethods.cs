d848102199d95041e269a03a490dc1136e49fae8
GuiAlquimim <galquimim04@gmail.com>
2026-04-13 09:20:28 -0300

    Exercício de fixação sobre Extension Methods concluído. Seção 14 finalizada!

---
+++ Secao14-ExtensionMet/ExFixacao-ExtensionMet/ExFixacao-ExtensionMet/Extensions/ExtensionsMethods.cs
@@ -0,0 +1,36 @@
+﻿namespace System
+{
+    internal static class ExtensionsMethods
+    {
+        //Converte uma string toda em minúsculo (ou misturada) para o formato de título (primeira letra de cada palavra em maiúsculo).
+        public static string ToTitleCase(this string thisObj)
+        {
+            string[] phrase = thisObj.Split(' ');
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                string word = phrase[i].ToLower();
+                if (word.Length > 0)
+                {
+                    phrase[i] = char.ToUpper(word[0]) + word.Substring(1);
+                }
+            }
+            return string.Join(" ", phrase);
+        }
+
+        //Compara a data do objeto com o DateTime.Now e retorna uma string legível.
+        public static string ToRelativeTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now - thisObj;
+            if (duration.TotalSeconds < 60)
+                return $"Just now";
+            else if (duration.TotalMinutes < 60)
+                return $"{(int)duration.TotalMinutes} minutes ago";
+            else if (duration.TotalHours < 24)
+                return $"{(int)duration.TotalHours} hours ago";
+            else if (duration.TotalHours < 48)
+                return $"Yesterday";
+            else return $"{(int)duration.TotalDays} days ago";
+        }
+    }
+}
---
+++ Secao14-ExtensionMet/ExFixacao-ExtensionMet/ExFixacao-ExtensionMet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ExFixacao_ExtensionMet
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = "TesTANDO MEu métODO de extensão to title case()";
+            Console.WriteLine(text);
+            Console.WriteLine(text.ToTitleCase());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Teste do método de extensão ToRelativeTime()");
+            DateTime date = DateTime.Now.Subtract(TimeSpan.FromDays(1));
+            Console.WriteLine(date.ToString("g", CultureInfo.GetCultureInfo("pt-BR")));
+            Console.WriteLine(date.ToRelativeTime());
+        }
+    }
+}
