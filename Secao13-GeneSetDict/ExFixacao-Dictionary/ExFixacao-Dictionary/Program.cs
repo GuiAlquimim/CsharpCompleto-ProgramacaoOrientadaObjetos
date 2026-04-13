9e5cde9f7371356e12fbab10f64e7ae3fa37aea1
GuiAlquimim <galquimim04@gmail.com>
2026-04-13 09:18:16 -0300

    Exercício de fixação sobre Dictionary concluído. Seção 13 finalizada!

---
+++ Secao13-GeneSetDict/ExFixacao-Dictionary/ExFixacao-Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ExFixacao_Dictionary
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = @"C:\Dev\CsharpCompleto-ProgramacaoOrientadaObjetos\Secao13-GeneSetDict\ExFixacao-Dictionary\Files\Votes registration.txt";
+
+            Dictionary<string, int> votesRegistration = new Dictionary<string, int>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        if (votesRegistration.ContainsKey(name))
+                            votesRegistration[name] += votes;
+                        else
+                            votesRegistration.Add(name, votes);
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> item in votesRegistration)
+                {
+                    Console.WriteLine($"{item.Key}, {item.Value}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+        }
+    }
+}
